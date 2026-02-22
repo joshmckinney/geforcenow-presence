31bed88666403a2c359d4f419a9894f5af28790e
KarmaDevz <acgarciaa13@gmail.com>
2026-02-21 23:22:17 -0500

    Remove unused Epic Games API, general API inspection, and test files, and update .gitignore.

--- inspect_api.py
+++
@@ -1,8 +0,0 @@
-import requests
-import json
-
-url = "https://api.github.com/repos/KarmaDevz/GeForce-NOW-Rich-Presence/releases/latest"
-response = requests.get(url)
-data = response.json()
-
-print(json.dumps(data, indent=4))
